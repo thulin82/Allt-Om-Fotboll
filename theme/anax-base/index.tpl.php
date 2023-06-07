42aacb9a43c77792b39748e3c3a84af2001b6370
Markus Thulin <macky_b@hotmail.com>
2023-06-07 14:06:55 +0200

    Removed (unused) default configs

--- theme/anax-base/index.tpl.php
+++ theme/anax-base/index.tpl.php
@@ -44,14 +44,5 @@
 <script src='<?=$this->url->asset($val)?>'></script>
 <?php endforeach; endif; ?>
 
-<?php if(isset($google_analytics)): ?>
-<script>
-  var _gaq=[['_setAccount','<?=$google_analytics?>'],['_trackPageview']];
-  (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-  g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-  s.parentNode.insertBefore(g,s)}(document,'script'));
-</script>
-<?php endif; ?>
-
 </body>
 </html>
